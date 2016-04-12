a08eed25ca8c73b1b5906c74b150c984a864858e
Jimisito <jimm.gom456@gmail.com>
2016-04-12 00:18:12 -0500

    primer proyecto

---
+++ ejercicio02.rb
@@ -0,0 +1,16 @@
+n=20
+while n>0
+	puts n
+	n-=1
+end
+ puts"========="
+ puts"========="
+
+
+n=20
+while n>0
+	puts n
+	n-=1
+end
+gets()
+
---
+++ ejercicio03.rb
@@ -0,0 +1,9 @@
+regiones=["huanuco","apurimac","cusco","pasco"]
+puts "el region mas lindo es: "+regiones[0]
+puts "================"
+regiones [0..4].each { |regi| 
+        puts "mis regiones son: "+regi
+ }
+
+
+gets()
